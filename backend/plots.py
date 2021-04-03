30932b5c4274ff0d4d4dd720f59802a6ef620eec
Alex <67833191+adouglas24@users.noreply.github.com>
2021-04-03 13:33:17 -0500

    making better

--- backend/plots.py
+++ backend/plots.py
@@ -17,10 +17,28 @@ def firstPie():
     plt.axis('equal')  
     plt.tight_layout()
 
-    plt.savefig("./images1/piechart1")
+    plt.savefig("./images1/piechart")
+    plt.close()
 
 def secondPie():
-    return 
+    labels = 'Technology', 'Financials'
+    sizes = [99.1, 0.9]
+    explode = (0.03, 0.03) 
+
+    colors = ['#3137FD','#50FFB1']
+
+    plt.pie(sizes, colors = colors, labels=labels, autopct='%1.1f%%', startangle=90, pctdistance=0.85, explode = explode)
+
+    centre_circle = plt.Circle((0,0),0.70,fc='white')
+    fig = plt.gcf()
+    fig.gca().add_artist(centre_circle)
+    # Equal aspect ratio ensures that pie is drawn as a circle
+    plt.axis('equal')  
+    plt.tight_layout()
+
+    plt.savefig("./images2/piechart")
+    plt.close()
+
 
 firstPie()
 
