00cce9a525e5ffaf2e966afa67871fd691fc030a
Daniel Li <daniel442li@gmail.com>
2021-04-03 15:49:52 -0500

    flask

---
+++ backend.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home_page():
+    return render_template('main.html')
+
+@app.route('/selections')
+def selections():
+    return render_template('selections.html')
+
+if __name__ == "__main__":
+    app.run(debug=True,port=5000)
